dfaa7cf8f5163d975c5f9e79f8f6e9cf38e612c6
pie4ro <pieroalexismendozasantos@gmail.com>
2026-02-23 23:42:20 -0500

    feat: modificacion de api_gateway añadiendo cache_cluster para guardar repsuestas

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -139,6 +139,8 @@ resource "aws_api_gateway_stage" "main" {
   deployment_id         = aws_api_gateway_deployment.main.id
   rest_api_id           = aws_api_gateway_rest_api.main.id
   stage_name            = var.environment
+  cache_cluster_enabled = true
+  cache_cluster_size    = "0.5"
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api.arn
