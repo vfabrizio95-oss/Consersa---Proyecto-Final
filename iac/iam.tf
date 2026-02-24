d892c816d7371c1f46dc3fb4908b8eb67e3ae068
pie4ro <pieroalexismendozasantos@gmail.com>
2026-02-23 23:09:11 -0500

    feat: creacion del rol iam y sus archivos

---
+++ iac/iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "apigw_cloudwatch" {
+  name = "${local.prefix}-apigw-cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "apigateway.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apigw_cloudwatch_attach" {
+  role       = aws_iam_role.apigw_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_api_gateway_account" "main" {
+  cloudwatch_role_arn = aws_iam_role.apigw_cloudwatch.arn
+}
