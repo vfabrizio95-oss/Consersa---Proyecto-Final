d135d72444b235c5254dc25db85e3e7f0358d6cb
Danna <vasquezdanna998@gmail.com>
2026-02-26 19:45:50 -0500

    fix: politica de encabezados de seguridad (CKV2_AWS_32)

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -21,6 +21,7 @@ resource "aws_cloudfront_distribution" "frontend" {
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
 
     forwarded_values {
       query_string = false
@@ -73,4 +74,22 @@ resource "aws_cloudfront_origin_access_control" "frontend" {
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_response_headers_policy" "security_headers" {
+  name = "${local.prefix}-security-headers"
+
+  security_headers_config {
+    content_type_options { override = true }
+    frame_options { frame_option = "DENY" override = true }
+    referrer_policy { referrer_policy = "strict-origin-when-cross-origin" override = true }
+    xss_protection { protection = true mode_block = true override = true }
+
+    strict_transport_security {
+      access_control_max_age_sec = 63072000
+      include_subdomains = true
+      preload = true
+      override = true
+    }
+  }
 }
