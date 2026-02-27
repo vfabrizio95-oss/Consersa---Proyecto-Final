877aa687933906dc47bba487bfa23542d01d42a9
vfabrizio95 <vfabrizio95@gmail.com>
2026-02-26 20:10:13 -0500

    fix: error de sintaxis en el archivo cloudfront

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -80,10 +80,22 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
   name = "${local.prefix}-security-headers"
 
   security_headers_config {
-    content_type_options { override = true }
-    frame_options { frame_option = "DENY" override = true }
-    referrer_policy { referrer_policy = "strict-origin-when-cross-origin" override = true }
-    xss_protection { protection = true mode_block = true override = true }
+    content_type_options {
+      override = true
+      }
+    frame_options {
+      frame_option = "DENY"
+      override = true
+      }
+    referrer_policy {
+      referrer_policy = "strict-origin-when-cross-origin"
+      override = true
+      }
+    xss_protection {
+      protection = true
+      mode_block = true
+      override = true
+      }
 
     strict_transport_security {
       access_control_max_age_sec = 63072000
