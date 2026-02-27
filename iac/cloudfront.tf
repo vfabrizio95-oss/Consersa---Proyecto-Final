f14471fc5822fcebb025285228384ce497a73aab
Danna <vasquezdanna998@gmail.com>
2026-02-26 19:23:03 -0500

    fix: aplicamos la version minima del TLS (error CKV_AWS_174)

--- iac/cloudfront.tf
+++ iac/cloudfront.tf
@@ -43,8 +43,10 @@ resource "aws_cloudfront_distribution" "frontend" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+  acm_certificate_arn      = aws_acm_certificate.cert.arn
+  ssl_support_method       = "sni-only"
+  minimum_protocol_version = "TLSv1.2_2021"
+}
 
   custom_error_response {
     error_code            = 404
