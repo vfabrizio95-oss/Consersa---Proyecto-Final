2697d457ec5275f389b43ba7f68a280df51e002f
vfabrizio95 <vfabrizio95@gmail.com>
2026-02-26 22:48:04 -0500

    fix: Se actualiza el bucket frontend para usar cifrado KMS por defecto en lugar de AES256 cumpliendo CKV_AWS_145.

--- iac/s3.tf
+++ iac/s3.tf
@@ -79,7 +79,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "frontend" {
   bucket = aws_s3_bucket.frontend.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.main.arn
     }
   }
 }
